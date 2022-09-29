83717b27eb10c84ed005e43e67ab31ddded3fcf2
Alannah Cullinane Cooney <63670829+AlannahCooney@users.noreply.github.com>
2022-09-29 16:45:50 +0100

    Add files via upload

---
+++ MergePDFfiles/mergePDF.py
@@ -0,0 +1,19 @@
+from PyPDF2 import PdfMerger
+
+""" 
+Author: Alannah Cullinane Cooney
+Date: 29/09/2022
+
+Create a list with the pdfs that you would like to merge
+For example I am using these two files from an Agile Processes lecture
+"""
+
+pdfs = ["chapter1.pdf", "Chapter2.pdf"]
+
+merger = PdfMerger()
+
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write("merged.pdf")
+merger.close()
