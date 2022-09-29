3db780df66e75090b3f69565310c77f0f92d605d
Alannah Cullinane Cooney <63670829+AlannahCooney@users.noreply.github.com>
2022-09-29 16:46:43 +0100

    Update mergePDF.py

--- MergePDFfiles/mergePDF.py
+++ MergePDFfiles/mergePDF.py
@@ -6,6 +6,8 @@ Date: 29/09/2022
 
 Create a list with the pdfs that you would like to merge
 For example I am using these two files from an Agile Processes lecture
+
+Add the PDf files that you would like to merge into the python directory that you are using!
 """
 
 pdfs = ["chapter1.pdf", "Chapter2.pdf"]
